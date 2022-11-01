69efba458f226dfe2d28afb2e60cbad4544cdec4
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2022-11-01 18:00:24 -0300

    Criação: Criado o componente de botão

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,1 +1,9 @@
-export {};
+import { ButtonHTMLAttributes } from "react";
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export const ButtonComponent = ({ children, ...rest }: IButton) => {
+  return <button {...rest}>{children}</button>;
+};
