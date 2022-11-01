40932045a9687945ca03d916a9de527fc2531622
Jalles <jallessantos12@gmail.com>
2022-11-01 10:17:00 -0300

    criacao: Adicionado pagina do portifolio e seu styles.ts base

---
+++ src/pages/PortFormPage/index.tsx
@@ -0,0 +1,12 @@
+import PortFormStyled from "./styles";
+
+const PortFormPage = () => {
+  console.log();
+  return (
+    <PortFormStyled>
+      <h1>Teste</h1>
+    </PortFormStyled>
+  );
+};
+
+export default PortFormPage;
---
+++ src/pages/PortFormPage/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const PortFormStyled = styled.form`
+  background-color: grey;
+`;
+
+export default PortFormStyled;
