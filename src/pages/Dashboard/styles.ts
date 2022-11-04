d4f8d344febebc0829dd9ce38a898aeda0011773
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2022-11-04 14:44:33 -0300

    fix: removendo import inútil

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import backGroundBtn from "../../assets/img/backgroundBtn.png";
 import { FcEditImage, FcEmptyTrash, FcShare } from "react-icons/fc";
 
 interface IStyledProps {
