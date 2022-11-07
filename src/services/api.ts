14354b0463252eaa5a73b99ff0b1b47829b48a8b
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2022-11-07 18:42:40 -0300

    fix: caminho da api e time toastify

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,6 @@ const token: string | null = localStorage.getItem("@PortGeek:token");
 
 export const api = axios.create({
   baseURL: "https://portgeek.herokuapp.com/",
-  timeout: 3500,
+  timeout: 10000,
   headers: { Authorization: `Bearer ${token}` },
 });
