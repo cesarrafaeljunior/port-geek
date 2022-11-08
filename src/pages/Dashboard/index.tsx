03bdb989e055b6d60cd7bb9ad837694048b378ec
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2022-11-08 20:23:35 -0300

    feat: link ao clicar na imagem

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -65,13 +65,28 @@ export const Dashboard = () => {
         {portfolioInfo?.id ? (
           <main>
             {portfolioInfo.project.selected_layout === "layout-1" && (
-              <img className="mini-port" src={imgPortfolio1} alt="" />
+              <img
+                className="mini-port"
+                src={imgPortfolio1}
+                alt=""
+                onClick={() => window.location.assign(shareLink)}
+              />
             )}
             {portfolioInfo.project.selected_layout === "layout-2" && (
-              <img className="mini-port" src={imgPortfolio2} alt="" />
+              <img
+                className="mini-port"
+                src={imgPortfolio2}
+                alt=""
+                onClick={() => window.location.assign(shareLink)}
+              />
             )}
             {portfolioInfo.project.selected_layout === "layout-3" && (
-              <img className="mini-port" src={imgPortfolio3} alt="" />
+              <img
+                className="mini-port"
+                src={imgPortfolio3}
+                alt=""
+                onClick={() => window.location.assign(shareLink)}
+              />
             )}
             <div className="buttons">
               <IconEdit onClick={() => setEditPortAuth(true)} />
