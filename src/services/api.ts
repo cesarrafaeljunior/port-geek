fb2579e8ded087c24e4dff527ecf2aad1068f3c2
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2022-11-09 09:25:07 -0300

    fix: captação do token para fazer o Patch

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const token = localStorage.getItem("@PortGeek:token") || "";
+export const token: string | null = localStorage.getItem("@PortGeek:token");
 export const api = axios.create({
   baseURL: "https://portgeek.herokuapp.com/",
   timeout: 10000,
